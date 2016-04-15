bbe9c3a421f43612a97ed23653916b1cd591ceb4
Zak <zak.damagedhair@gmail.com>
2016-04-15 17:25:48 +0800

    ccc

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Origin", "null");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
